886802381637233134d68afd470feab8618fbc60
Usuario <Usuario@DESKTOP-PB8P1A7>
2024-06-02 20:15:17 -0500

    Agregar archivos de proyecto.

---
+++ SebastianCruzPersonalizacionMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SebastianCruzPersonalizacionMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SebastianCruzPersonalizacionMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SebastianCruzPersonalizacionMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
